beb14be7464a4be1eaaceaa977df2cbfcc2f37e0
Piyush <piyushpatidar5@gmail.com>
2020-04-25 15:10:29 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header @HandleLinkSelection="NavigateTo"/>
+    <Body/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Body from "./components/Body.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Body,
+    Footer
+  },
+  methods: {
+    NavigateTo(selection) {
+	console.log(selection);
+	}
+  }
+};
+</script>
+
+<style>
+</style>
